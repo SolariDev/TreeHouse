876f46f9a7007919622e4e437fa78397f3a49609
SolariDev <gabrielsolari75@gmail.com>
2025-08-22 14:28:04 -0300

    estructura base sin estilos

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Extensions.Logging;
+
+namespace TreeHouse;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new NavigationPage(new MainPage()));
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace TreeHouse;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnRegisterClicked(object sender, EventArgs e)
+	{
+		await DisplayAlert("Registro", "Pantalla en construcción", "OK");
+	}
+
+	private async void OnLoginClicked(object sender, EventArgs e)
+	{
+		await DisplayAlert("Inicio de sesión", "Pantalla en construcción", "OK");
+	}
+}
+
